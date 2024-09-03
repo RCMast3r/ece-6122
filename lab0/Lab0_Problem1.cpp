c7aba0ab4460fa72e6d4bfe6a309c17f68db1b3e
Ben Hall <rcmast3r1@gmail.com>
2024-09-03 09:16:23 -0400

    labs

---
+++ lab0/Lab0_Problem1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "My name is: Ben Hall" << std::endl;
+    std::cout << "This (\") is a double quote." << std::endl;
+    std::cout << "This (`) is a single quote." << std::endl;
+    std::cout << "This (\\) is a backslash." << std::endl;
+    std::cout << "This (/) is a forward slash." << std::endl;
+    return 0;
+}
