f7ad8c7f01aa76fb661450c68a7e3b3efb02c632
Ben Hall <rcmast3r1@gmail.com>
2024-10-12 22:41:15 -0400

    updated game

--- lab2/include/GameOfLife.hpp
+++ lab2/include/GameOfLife.hpp
@@ -11,58 +11,89 @@
 #include <barrier>
 #include <condition_variable>
 #include <omp.h>
-class GameOfLife {
+class GameOfLife
+{
   public:
+    /// @brief constructor for the game of life class
+    /// @param threadingModelIndex the index derived from the command line arguments to determine what threading mode to use
+    /// @param numThreads the number of threads to spawn if under a threading supported mode
+    /// @param width width derived from window width, height and cell size
+    /// @param height height derived from window width, height and cell size
     explicit GameOfLife(int threadingModelIndex, std::size_t numThreads,
                         std::size_t width, std::size_t height);
+
+    /// @brief handler that calls the internal methods for updating the grid. calls the one that was specified to use (SEQ/THRD/OMP)
     void updateGrid();
 
+    /// @brief getter for grids
+    /// @return pair of references to the grids
     std::pair<std::vector<std::vector<bool>> &,
               std::vector<std::vector<bool>> &>
-    getRefs() {
+    getRefs()
+    {
         return {_prevGrid, _grid};
     }
-    void init() {
+    /// @brief handles the intialization of the game of life class. will
+    /// conditionally start all of the std::threads
+    void init()
+    {
 
         _generateGrid(_grid);
-        if (_threadingModelIndex == 1) {
-            for (int i = 0; (i < _numThreads); i++) {
+        if (_threadingModelIndex == 1)
+        {
+            for (int i = 0; (i < _numThreads); i++)
+            {
                 _threads.push_back(
                     std::thread(&GameOfLife::_handleGridUpdate, this, i));
             }
-        } else if (_threadingModelIndex == 2) {
-            // omp_set_dynamic(0);
-            // omp_set_num_threads(_numThreads);
         }
     }
 
   private:
+    /// @brief handler for single threaded grid updating
     void _updateGridSEQ();
+    
+    /// @brief the handler for kicking the multi-threaded grid update threads. waits for the threads to finish 
+    //         by waiting on a second condition variable kicked by a std::barrier handler function
     void _updateGridThreaded();
-
+    
+    /// @brief openmp multi-threading handler for grid updating. 
+    /// @param numThreads the number of threads to use
     void _updateGridOpenMPThreaded(std::size_t numThreads);
 
-    void _onGridPopulation() {
+    /// @brief handler given to the std::barrier for signaling that all the std::threads have finished
+    void _onGridPopulation()
+    {
         {
             std::unique_lock lk(_retMtx);
             _gridPopulated = true;
         }
-        if (_count++ % 2) {
+        if (_count++ % 2)
+        {
             _prevGridRef = _grid;
             _gridRef = _prevGrid;
         }
         _retCv.notify_all();
     }
+
+    /// @brief grid generator for randomizing grid initially
+    /// @param grid the grid to initialize
     void _generateGrid(std::vector<std::vector<bool>> &grid);
+
+    /// @brief the grid updating function that gets run in the thread. has a while(true) loop for staying alive and waits on a condition variable to start
+    /// @param index the index of this thread. gets used with the for statement to 
     void _handleGridUpdate(int index);
     static inline int
     _countNeighbors(const std::vector<std::vector<bool>> &grid, std::size_t x,
-                    std::size_t y, std::size_t width, std::size_t height) {
+                    std::size_t y, std::size_t width, std::size_t height)
+    {
         int count = 0;
-        for (int dx = -1; dx <= 1; ++dx) {
+        for (int dx = -1; dx <= 1; ++dx)
+        {
             size_t nx = (x + dx + width) % width;
             const auto &col = grid[nx]; // get the column once
-            for (int dy = -1; dy <= 1; ++dy) {
+            for (int dy = -1; dy <= 1; ++dy)
+            {
                 if (dx == 0 && dy == 0) continue;
                 size_t ny = (y + dy + height) % height;
 
@@ -71,6 +102,7 @@ class GameOfLife {
         }
         return count;
     }
+
   private:
     std::barrier<std::function<void()>> _threadSync;
     std::mutex _gridMtx, _retMtx;
--- lab2/src/GameOfLife.cpp
+++ lab2/src/GameOfLife.cpp
@@ -15,7 +15,8 @@ GameOfLife::GameOfLife(int threadingModelIndex, std::size_t numThreads,
                   std::function<void()>([this]() { _onGridPopulation(); })),
       _grid(width, std::vector<bool>(height, false)),
       _prevGrid(width, std::vector<bool>(height, false)),
-      _prevGridRef(_prevGrid), _gridRef(_grid) {
+      _prevGridRef(_prevGrid), _gridRef(_grid)
+{
     _numThreads = numThreads;
     // set the number of threads globally for openmp (only matters if running
     // under openmp mode)
@@ -25,12 +26,15 @@ GameOfLife::GameOfLife(int threadingModelIndex, std::size_t numThreads,
     _threadingModelIndex = threadingModelIndex;
 }
 
-void GameOfLife::_generateGrid(std::vector<std::vector<bool>> &grid) {
+void GameOfLife::_generateGrid(std::vector<std::vector<bool>> &grid)
+{
     std::cout << " width: " << _gridWidth << std::endl;
     std::cout << " height: " << _gridHeight << std::endl;
     std::srand(static_cast<unsigned>(std::time(nullptr)));
-    for (std::size_t x = 0; x < _gridWidth; ++x) {
-        for (std::size_t y = 0; y < _gridHeight; ++y) {
+    for (std::size_t x = 0; x < _gridWidth; ++x)
+    {
+        for (std::size_t y = 0; y < _gridHeight; ++y)
+        {
             auto test = std::rand() % 2;
 
             grid[x][y] = (test == 0); // Randomly seed each pixel
@@ -38,63 +42,78 @@ void GameOfLife::_generateGrid(std::vector<std::vector<bool>> &grid) {
     }
 }
 
-void GameOfLife::updateGrid() {
-    switch (_threadingModelIndex) {
-    case 0: {
+void GameOfLife::updateGrid()
+{
+    switch (_threadingModelIndex)
+    {
+    case 0:
+    {
         _updateGridSEQ();
         break;
     }
-    case 1: {
+    case 1:
+    {
         _updateGridThreaded();
         break;
     }
-    case 2: {
+    case 2:
+    {
         _updateGridOpenMPThreaded(_numThreads);
-        
+
         break;
     }
     }
 }
 
-void GameOfLife::_updateGridSEQ() {
-    for (int x = 0; x < _gridWidth; ++x) {
-        for (int y = 0; y < _gridHeight; ++y) {
+void GameOfLife::_updateGridSEQ()
+{
+    for (int x = 0; x < _gridWidth; ++x)
+    {
+        for (int y = 0; y < _gridHeight; ++y)
+        {
             int neighbors =
                 _countNeighbors(_prevGridRef, x, y, _gridWidth, _gridHeight);
-            // std::cout << "x " << x << std::endl;
-            // std::cout << "y " << y << std::endl;
-            if (_prevGridRef[x][y]) {
-                if (neighbors < 2 || neighbors > 3) {
+            if (_prevGridRef[x][y])
+            {
+                if (neighbors < 2 || neighbors > 3)
+                {
                     _gridRef[x][y] = false; // Cell dies
                 }
-            } else {
-                if (neighbors == 3) {
+            }
+            else
+            {
+                if (neighbors == 3)
+                {
                     _gridRef[x][y] = true; // Cell becomes alive
                 }
             }
         }
     }
-    if (_count++ % 2) {
+    if (_count++ % 2)
+    {
         _prevGridRef = _grid;
         _gridRef = _prevGrid;
     }
 }
 
-void GameOfLife::_handleGridUpdate(int index) {
+void GameOfLife::_handleGridUpdate(int index)
+{
     int thisThreadIndex = index;
 
-    auto verticalWidthSize = std::floor(_gridWidth / _numThreads);
+    auto widthSize = std::floor(_gridWidth / _numThreads);
 
-    auto startInd = (verticalWidthSize * thisThreadIndex);
+    auto startInd = (widthSize * thisThreadIndex);
 
     // handle small last little bit that does not get updated. in the last
     // thread we need to increase the width size to get to the rest of the
     // screen
-    if (thisThreadIndex == (_numThreads - 1)) {
-        verticalWidthSize = _gridWidth - startInd;
+    if (thisThreadIndex == (_numThreads - 1))
+    {
+        widthSize = _gridWidth - startInd;
     }
 
-    while (true) {
+    while (true)
+    {
         {
             std::unique_lock lk(_gridMtx);
             _cv.wait(lk, [this] { return _startPopulateGrid; });
@@ -103,27 +122,36 @@ void GameOfLife::_handleGridUpdate(int index) {
         auto prevGridBegin = _prevGridRef.begin();
         auto gridBegin = _gridRef.begin();
 
-        for (int x = 0; x < verticalWidthSize; ++x) {
+        for (int x = 0; x < widthSize; ++x)
+        {
             std::size_t x_loc = startInd + x;
-            for (int y = 0; y < _gridHeight; ++y) {
+            for (int y = 0; y < _gridHeight; ++y)
+            {
                 int neighbors = _countNeighbors(_prevGridRef, x_loc, y,
                                                 _gridWidth, _gridHeight);
-                if (_prevGridRef[x_loc][y]) {
-                    if (neighbors < 2 || neighbors > 3) {
+                if (_prevGridRef[x_loc][y])
+                {
+                    if (neighbors < 2 || neighbors > 3)
+                    {
                         _gridRef[x_loc][y] = false; // Cell dies
                     }
-                } else {
-                    if (neighbors == 3) {
+                }
+                else
+                {
+                    if (neighbors == 3)
+                    {
                         _gridRef[x_loc][y] = true; // Cell becomes alive
                     }
                 }
             }
         }
+        // use the std::barrier synchronization primitive. this gets called only once after all threads have finished
         _threadSync.arrive_and_wait();
     }
 }
 
-void GameOfLife::_updateGridThreaded() {
+void GameOfLife::_updateGridThreaded()
+{
     _gridPopulated = false;
 
     {
@@ -139,29 +167,45 @@ void GameOfLife::_updateGridThreaded() {
     }
 }
 
-void GameOfLife::_updateGridOpenMPThreaded(std::size_t numThreads) {
-    int x;
-    #pragma omp parallel for num_threads(numThreads)
-       for (x = 0; x < _gridWidth; ++x) {
-        for (int y = 0; y < _gridHeight; ++y) {
-            int neighbors =
-                _countNeighbors(_prevGridRef, x, y, _gridWidth, _gridHeight);
-            // std::cout << "x " << x << std::endl;
-            // std::cout << "y " << y << std::endl;
-            if (_prevGridRef[x][y]) {
-                if (neighbors < 2 || neighbors > 3) {
-                    _gridRef[x][y] = false; // Cell dies
+void GameOfLife::_updateGridOpenMPThreaded(std::size_t numThreads)
+{
+int startInd, verticalWidthSize;
+#pragma omp parallel num_threads(numThreads) private(startInd, verticalWidthSize)
+    {
+    int thisThreadIndex = omp_get_thread_num();  // the thread index
+    verticalWidthSize = std::ceil(_gridWidth / numThreads);
+    startInd = (verticalWidthSize * thisThreadIndex);
+    if (thisThreadIndex == (numThreads - 1))
+    {
+        verticalWidthSize = _gridWidth - startInd;
+    }
+#pragma omp parallel for
+    for (int x = 0; x < (int)verticalWidthSize; ++x)
+    {
+        std::size_t x_loc = startInd + x; // just like in the std::thread one
+        for (int y = 0; y < _gridHeight; ++y)
+        {
+            int neighbors = _countNeighbors(_prevGridRef, x_loc, y, _gridWidth, _gridHeight);
+            if (_prevGridRef[x_loc][y])
+            {
+                if (neighbors < 2 || neighbors > 3)
+                {
+                    _gridRef[x_loc][y] = false; // Cell dies
                 }
-            } else {
-                if (neighbors == 3) {
-                    _gridRef[x][y] = true; // Cell becomes alive
+            }
+            else
+            {
+                if (neighbors == 3)
+                {
+                    _gridRef[x_loc][y] = true; // Cell becomes alive
                 }
             }
         }
     }
-    if (_count++ % 2) {
+}
+    if (_count++ % 2)
+    {
         _prevGridRef = _grid;
         _gridRef = _prevGrid;
-    } 
-        
+    }
 }
