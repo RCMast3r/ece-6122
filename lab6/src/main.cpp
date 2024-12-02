6cd103e4d4a3c5653213724d560aca835a2a0a1a
Ben Hall <rcmast3r1@gmail.com>
2024-12-02 13:52:58 -0500

    asdf

--- lab6/src/main.cpp
+++ lab6/src/main.cpp
@@ -72,78 +72,51 @@ double getResult(std::function<double(double)> func, const std::vector<double>& 
 }
 
 int main(int argc, char *argv[]) {
-
-    // handle program options
     auto opts = getConfig(argc, argv);
-
-    // set up the random number generator and get values from it
-    std::random_device rd; // Will be used to obtain a seed for the random number engine
-    std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
+    std::random_device rd;
+    std::mt19937 gen(rd());
     std::uniform_real_distribution<> dis(0.0, 1.0);
 
-    auto xVals = std::vector<double>(opts.numSamples);
-    for (std::size_t n = 0; n < opts.numSamples; n++) {
-        xVals.at(n) = dis(gen);
-    }
-
-    // initialize MPI 
     MPI_Init(NULL, NULL);
-
-    // get the current task index and total number of world tasks
-    int worldRank;
+    int worldRank, worldSize;
     MPI_Comm_rank(MPI_COMM_WORLD, &worldRank);
-    int worldSize;
     MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
 
-    // broadcast all data to every task
-    MPI_Bcast(static_cast<void *>(xVals.data()), xVals.size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    double sub_avg = 0.0f;
-    // handle calculation of the results within each process
-    switch(static_cast<IntegralType>(opts.integralVersion))
-    {
+
+    auto xVals = std::vector<double>();
+    if (opts.numSamples > 0) xVals.resize(opts.numSamples);
+    if (worldRank == 0) {
+        for (std::size_t n = 0; n < opts.numSamples; n++) {
+            xVals.at(n) = dis(gen);
+        }
+    }
+
+        // Broadcast size and data
+    int size = static_cast<int>(opts.numSamples);
+    MPI_Bcast(&size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    if (worldRank != 0) xVals.resize(size);
+    MPI_Bcast(xVals.data(), size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    double sub_avg = 0.0;
+    switch (opts.integralVersion) {
         case IntegralType::PRIMARY:
-        {
             sub_avg = getResult(computePrimaryIntegral, xVals, worldRank, worldSize);
             break;
-        }
         case IntegralType::SECONDARY:
-        {
             sub_avg = getResult(computeSecondaryIntegral, xVals, worldRank, worldSize);
             break;
-        }
         default:
-        {
             sub_avg = getResult(computePrimaryIntegral, xVals, worldRank, worldSize);
-            break;
-        }
     }
 
-    // send from all processes the sub- average to be averaged
-    MPI_Send(static_cast<void *>(&sub_avg), 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+    double total_avg = 0.0;
+    MPI_Reduce(&sub_avg, &total_avg, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    std::vector<double> results;
-    if(worldRank == 0)
-    {
-        results.resize(worldSize);
-    }
-    // gather the results from the all tasks, including the root one.
-    MPI_Gather(static_cast<void *>(&sub_avg), 1, MPI_DOUBLE, static_cast<void*>(results.data()), 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    
-    // get the final average and print out the result 
     if (worldRank == 0) {
-        double sum =0;
-        for(const auto & r : results)
-        {
-            sum+=r;
-        }
-
-        double avg = sum / static_cast<double>(results.size());
-        std::cout << "The estimate for integral "<< static_cast<int>(opts.integralVersion) <<" is " << avg <<std::endl;
-        std::cout << "Bye!" <<std::endl;
-
+        std::cout << "Estimate for integral " << static_cast<int>(opts.integralVersion)
+                  << " is " << (total_avg / worldSize) << std::endl;
     }
-    
-    // Finalize the MPI environment.
+
     MPI_Finalize();
     return 0;
 }
